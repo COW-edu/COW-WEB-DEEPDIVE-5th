9a932a7a7978305ef0167d1936ec7eb5c4881251
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-04-02 13:27:31 +0900

    <Feat> mainLayout 구성 (outlet)

--- Todo/최원빈/TODOCHOI/src/router/router.tsx
+++ Todo/최원빈/TODOCHOI/src/router/router.tsx
@@ -3,10 +3,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import Homepage from '../pages/contents/HomePage';
 import NotFoundPage from '../pages/NotFoundPage';
 
+import MainLayout from '../layouts/MainLayout';
+
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: '', // 공통 레이아웃
+    element: <MainLayout />, // 공통 레이아웃
     children: [
       { path: '/', element: <Homepage /> },
       { path: '*', element: <NotFoundPage /> },
