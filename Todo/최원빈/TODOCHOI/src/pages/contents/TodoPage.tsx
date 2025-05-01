2d9c8654085a0291ea9b95b93bf86dea7545c06f
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-05-01 17:05:50 +0900

    <feat> input 분리 + 추가

--- Todo/최원빈/TODOCHOI/src/pages/contents/TodoPage.tsx
+++ Todo/최원빈/TODOCHOI/src/pages/contents/TodoPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '../../components/atomic/Button';
+import Input from '../../components/atomic/Input';
 const TodoPage = () => {
   //todoList Type
   type todoListItems = {
@@ -35,7 +36,7 @@ const TodoPage = () => {
         </h1>
         <div className="flex flex-col justify-center items-center">
           <div className="w-full max-w-6xl bg-cyan-100 px-6 py-4 rounded-md shadow flex justify-between items-center">
-            <input type="text" onChange={handleInputChange} />
+            <Input variant="todo" onChange={handleInputChange}></Input>
             <div className=" flex gap-2">
               <Button variant="append" onClick={addTodo}>
                 추가
@@ -46,9 +47,9 @@ const TodoPage = () => {
           </div>
           <div className="w-full bg-yellow-50 max-w-6xl">
             <div className="flex flex-col">
-              {todoList.map((val) => {
-                return <div key={val.id}>{val.text}</div>;
-              })}
+              {todoList.map((val) => (
+                <div key={val.id}>{val.text}</div>
+              ))}
             </div>
           </div>
         </div>
