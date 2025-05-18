d5e56d9ef56bf4cc3a0998304f4180e0e7a0160e
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-05-18 21:55:36 +0900

    학습 Md 파일 추가

--- Todo/최원빈/TODOCHOI/src/pages/TodoPage.tsx
+++ Todo/최원빈/TODOCHOI/src/pages/TodoPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Plus, Trash2, Github, Divide } from 'lucide-react';
+import { Plus, Trash2, Github } from 'lucide-react';
 
 import * as Checkbox from '@radix-ui/react-checkbox';
 import * as Tabs from '@radix-ui/react-tabs';
@@ -7,6 +7,7 @@ import { Check } from 'lucide-react';
 import Button from '../components/atomic/Button';
 import Input from '../components/atomic/Input';
 import { TodoListItem } from '../types/todoType';
+import todoList from './contents/TodoList';
 
 const TodoPage = () => {
   const [todoList, setTodoList] = useState<TodoListItem[]>([]);
@@ -124,34 +125,12 @@ const TodoPage = () => {
         <div className="space-y-3">
           {showTodoContent === 'all' ? (
             <div>
-              {todoList.map((todo) => (
-                <div
-                  key={todo.id}
-                  className="flex justify-between items-center bg-yellow-100 dark:bg-yellow-200 rounded-lg px-4 py-3 shadow"
-                >
-                  <Checkbox.Root
-                    className="w-5 h-5 bg-white border rounded flex  shadow"
-                    checked={todo.completed}
-                    onCheckedChange={() => toggleTodo(todo.id)}
-                  >
-                    <Checkbox.Indicator>
-                      <Check className="w-4 h-4 text-green-500" />
-                    </Checkbox.Indicator>
-                  </Checkbox.Root>
-
-                  <span
-                    className={`text-gray-800 font-medium ${
-                      todo.completed ? 'line-through text-red-400' : null
-                    }`}
-                    onClick={() => handleTextClick(todo.id)}
-                  >
-                    {todo.text}
-                  </span>
-                  <Button variant="reduce" onClick={() => deleteTodo(todo.id)}>
-                    <Trash2 className="w-4 h-4 mr-1" />
-                  </Button>
-                </div>
-              ))}
+              <todoList
+                todoList={todoList}
+                deleteTodo={deleteTodo}
+                toggleTodo={toggleTodo}
+                handleTextClick={handleTextClick}
+              ></todoList>
             </div>
           ) : (
             <div>
